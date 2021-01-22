18eccad84031a1d59a445e795cc92eb7afbcf8fe
poby123 <youngsm12@naver.com>
2021-01-22 11:36:10 +0900

    google oauth config

---
+++ server/config/passport.js
@@ -0,0 +1,30 @@
+let passport = require('passport');
+let GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+// Strategy 성공시 호출됨.
+passport.serializeUser(function(user, done) {
+  done(null, user); // 여기의 user가 desiralizeUser의 첫 번째 매개변수로 전달된다.
+});
+
+// 사용자가 요청할 때마다 호출된다.
+passport.deserializeUser(function(user, done) {
+  done(null, user); // 여기의 user가 req.user가 된다.
+});
+
+passport.use(new GoogleStrategy(
+  {
+    clientID      : GOOGLE_CLIENT_ID,
+    clientSecret  : GOOGLE_CLIENT_SECRET,
+    callbackURL   : '/auth/google/callback',
+    passReqToCallback   : true
+  }, function(request, accessToken, refreshToken, profile, done){
+    console.log('profile: ', profile);
+    let user = profile;
+    done(null, user);
+  }
+));
+
+module.exports = passport;
