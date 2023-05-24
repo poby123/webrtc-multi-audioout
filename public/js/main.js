6b6a02bf3860683c5e155b7bd404d8f3913d2742
poby123 <50279318+poby123@users.noreply.github.com>
2023-05-24 23:48:04 +0900

    style: 수락/거절

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,7 @@ if (!myName) {
 
   do {
     name = prompt('참여할 이름을 정해주세요 : ');
-  } while (!name)
+  } while (!name);
 
   myName = name;
   myProfile = '/images/default_profile.webp';
@@ -34,7 +34,6 @@ const audioOutputSelect = document.querySelector('select#audioOutputSource');
 const videoSelect = document.querySelector('select#videoSource');
 const selectors = [audioInputSelect, audioOutputSelect, videoSelect];
 
-
 function init() {
   socket = io();
 
@@ -47,7 +46,7 @@ function init() {
   socket.on('host', (updatedStatus) => {
     myInfo.status = updatedStatus;
   });
-  
+
   socket.on('requestJoin', (userInfo) => {
     window.focus();
     console.log('request Join');
@@ -115,9 +114,9 @@ function addPeer(am_initiator, userInfo) {
     initiator: am_initiator,
     stream: localStream,
     config: configuration,
-    sdpTransform: (sdp) => { 
+    sdpTransform: (sdp) => {
       sdp.replace('useinbandfec=1', 'useinbandfec=1; stereo=1; maxaveragebitrate=510000');
-      return sdp 
+      return sdp;
     },
   });
 
@@ -138,7 +137,7 @@ function addPeer(am_initiator, userInfo) {
   /*****************************/
   peers[id].on('stream', (stream) => {
     addPeerList(id, userInfo);
-    createParticipantsContainer(id, userInfo, stream)
+    createParticipantsContainer(id, userInfo, stream);
   });
 }
 
@@ -147,7 +146,6 @@ function addPeer(am_initiator, userInfo) {
  * Removes the video element and deletes the connection
  */
 function removePeer(sessionId) {
-
   if (streams[sessionId]) {
     if (sessionId == currentMaximizedId) {
       handleMinimize();
@@ -155,9 +153,8 @@ function removePeer(sessionId) {
     delete streams[sessionId];
   }
 
-  let videoEl = document.getElementById(sessionId);
-  let videoContainer = document.getElementById(`container_${sessionId}`);
-  let meter = document.getElementById(`meter_${sessionId}`);
+  const videoEl = document.getElementById(sessionId);
+  const videoContainer = document.getElementById(`container_${sessionId}`);
 
   if (videoEl?.srcObject) {
     const tracks = videoEl.srcObject.getTracks();
@@ -167,29 +164,31 @@ function removePeer(sessionId) {
     });
 
     videoEl.srcObject = null;
-    videoContainer.removeChild(videoEl);
-    videoContainer.removeChild(meter);
-    videoContainer.parentNode.removeChild(videoContainer);
   }
+
+  while (videoContainer.hasChildNodes()) {
+    videoContainer.removeChild(videoContainer.firstChild);
+  }
+  videoContainer.parentNode.removeChild(videoContainer);
+
   if (peers[sessionId]) {
     peers[sessionId].destroy();
     delete peers[sessionId];
   }
-  
+
   console.log('meter : ', meterRefreshs[sessionId]);
   meterRefreshs[sessionId] && delete meterRefreshs[sessionId];
-  
+
   deletePeerList(sessionId);
   deleteWaitList(sessionId);
 }
-
 
 function switchMedia() {
   const audioSource = audioInputSelect.value;
   const videoSource = videoSelect.value;
   constraints.audio = { ...constraints.audio, deviceId: audioSource ? { exact: audioSource } : undefined };
   constraints.video = { ...constraints.video, deviceId: videoSource ? { exact: videoSource } : undefined };
-  
+
   const tracks = localStream.getTracks();
   const videoState = localStream.getVideoTracks()[0].enabled;
   const audioState = localStream.getAudioTracks()[0].enabled;
@@ -224,7 +223,6 @@ function switchMedia() {
   });
 }
 
-
 function removeLocalStream() {
   if (localStream) {
     const tracks = localStream.getTracks();
@@ -241,7 +239,6 @@ function removeLocalStream() {
   }
 }
 
-
 function handleApprove(e) {
   const id = e.currentTarget.id;
   const targetInfo = waitUsers[id];
@@ -249,14 +246,12 @@ function handleApprove(e) {
   deleteWaitList(id);
 }
 
-
 function handleReject(e) {
   const id = e.currentTarget.id;
   const targetInfo = waitUsers[id];
   socket.emit('requestJoin', targetInfo, false, id, roomId);
   deleteWaitList(id);
 }
-
 
 async function getDevices() {
   const devices = await navigator.mediaDevices.enumerateDevices();
@@ -272,24 +267,22 @@ async function getDevices() {
       select.removeChild(select.firstChild);
     }
   });
-  for (const {deviceId, label, kind} of devices) {
+  for (const { deviceId, label, kind } of devices) {
     const option = document.createElement('option');
     option.value = deviceId;
 
-    (currentAudioInputDeviceId === deviceId) && (option.selected = true);
-    (currentVideoInputDeviceId === deviceId) && (option.selected = true);
+    currentAudioInputDeviceId === deviceId && (option.selected = true);
+    currentVideoInputDeviceId === deviceId && (option.selected = true);
 
     if (kind === 'audioinput') {
       option.text = label || `microphone ${audioInputSelect.length + 1}`;
       audioInputSelect.appendChild(option);
-    } 
-    else if (kind === 'videoinput') {
+    } else if (kind === 'videoinput') {
       option.text = label || `camera ${videoSelect.length + 1}`;
       videoSelect.appendChild(option);
-    }
-    else if(kind === 'audiooutput'){
-      option.text = label || `speaker ${audioOutputSelect.length + 1}`
-      audioOutputSelect.appendChild(option)
+    } else if (kind === 'audiooutput') {
+      option.text = label || `speaker ${audioOutputSelect.length + 1}`;
+      audioOutputSelect.appendChild(option);
     }
   }
 
@@ -300,7 +293,6 @@ async function getDevices() {
   });
 }
 
-
 async function getStream() {
   const stream = await navigator.mediaDevices.getUserMedia(constraints);
   localStream = stream;
@@ -309,23 +301,21 @@ async function getStream() {
 }
 
 async function start() {
-  try{
+  try {
     console.log('start!');
     await getDevices();
     await getStream();
     toggleMute();
-  }
-  catch(e){
+  } catch (e) {
     console.log(e);
-  }
-  finally{
+  } finally {
     init();
   }
 }
 
-window.onload = function() {
+window.onload = function () {
   audioInputSelect.onchange = switchMedia;
   videoSelect.onchange = switchMedia;
   audioOutputSelect.onchange = handleSoundChange;
   start();
-}
+};
--- public/js/ui_control.js
+++ public/js/ui_control.js
@@ -10,269 +10,312 @@ let showConfigModal = false;
 let showUserList = false;
 
 function handleMaximize(e) {
-    const targetElement = e.currentTarget;
-    const id = e.currentTarget.id.replace('maximizeButton_', '');
+  const targetElement = e.currentTarget;
+  const id = e.currentTarget.id.replace('maximizeButton_', '');
 
-    // toggle maximized section
-    if(currentMaximizedId === id){
-        handleMinimize(e);
-        return;
-    }
+  // toggle maximized section
+  if (currentMaximizedId === id) {
+    handleMinimize(e);
+    return;
+  }
 
-    targetElement.src = '/fonts/minimize-solid.svg' 
+  targetElement.src = '/fonts/minimize-solid.svg';
 
-    currentMaximizedId = id;
-    const selectedContainer = document.getElementById(`container_${id}`);
-    maximizedVideoSection.appendChild(selectedContainer);
-    maximizedVideoSection.setAttribute('style', 'display: flex');
-    videosConainerGroup.setAttribute('style', 'display: none');
+  currentMaximizedId = id;
+  const selectedContainer = document.getElementById(`container_${id}`);
+  maximizedVideoSection.appendChild(selectedContainer);
+
+  $('.video-container-group').css('display', 'none');
+  $('.section-maximized-video').css('display', 'flex');
+  hideAllModal();
 }
 
 /**
  * It is invoked by handleMaximize function
- * @param {*} e 
- * @returns 
+ * @param {*} e
+ * @returns
  */
 function handleMinimize(e) {
-    if(!currentMaximizedId){
-        alert('[ERROR]: cannot minimize due current maximized value is not defined.');
-        return;
-    }
+  if (!currentMaximizedId) {
+    console.error('[ERROR]: cannot minimize due current maximized value is not defined.');
+    return;
+  }
 
-    const id = currentMaximizedId;
-    currentMaximizedId = null;
-    
-    const selectedContainer = document.getElementById(`container_${id}`);
-    videosConainerGroup.appendChild(selectedContainer);
+  const id = currentMaximizedId;
+  currentMaximizedId = null;
 
-    while(maximizedVideoSection.hasChildNodes()){
-        maximizedVideoSection.removeChild(maximizedVideoSection.firstChild);
-    }
+  const selectedContainer = document.getElementById(`container_${id}`);
+  videosConainerGroup.appendChild(selectedContainer);
 
+  while (maximizedVideoSection.hasChildNodes()) {
+    maximizedVideoSection.removeChild(maximizedVideoSection.firstChild);
+  }
+
+  if (e) {
     const targetElement = e.currentTarget;
-    targetElement.src = '/fonts/maximize-solid.svg'; 
+    targetElement.src = '/fonts/maximize-solid.svg';
+  }
 
-    videosConainerGroup.setAttribute('style', 'display: flex');
-    maximizedVideoSection.setAttribute('style', 'display: none');
+  $('.video-container-group').css('display', 'flex');
+  $('.section-maximized-video').css('display', 'none');
 }
 
 function toggleAudioOutput(e) {
-    const targetElement = e.currentTarget;
-    const id = targetElement.id.replace('speakerButton_', '');
+  const targetElement = e.currentTarget;
+  const id = targetElement.id.replace('speakerButton_', '');
 
-    const targetVideo = document.getElementById(`${id}`);
-    const currentMuteStatus = targetVideo.muted;
+  const targetVideo = document.getElementById(`${id}`);
+  const currentMuteStatus = targetVideo.muted;
 
-    // change status
-    targetVideo.muted = !currentMuteStatus;
-    if(targetVideo.muted){
-        targetElement.src = '/fonts/volume-xmark.svg';
-    }
-    else{
-        targetElement.src = '/fonts/volume-high.svg';
-    }
+  // change status
+  targetVideo.muted = !currentMuteStatus;
+  if (targetVideo.muted) {
+    targetElement.src = '/fonts/volume-xmark.svg';
+  } else {
+    targetElement.src = '/fonts/volume-high.svg';
+  }
 }
 
-function toggleVideoOutput(e){
-    const targetElement = e.currentTarget;
-    const id = targetElement.id.replace('videoOutputToggleButton_', '');
-    const targetVideo = document.getElementById(`${id}`);
-    
-    // change status
-    targetVideo.srcObject = targetVideo.srcObject ? undefined : streams[id];
-    if(!targetVideo.srcObject){
-        targetElement.src = '/fonts/mint-video-slash.svg';
-    }
-    else{
-        targetElement.src = '/fonts/mint-video.svg';
-    }
+/**
+ * @param {*} e
+ */
+function toggleVideoOutput(e) {
+  const targetElement = e.currentTarget;
+  const id = targetElement.id.replace('videoOutputToggleButton_', '');
+  const targetVideo = document.getElementById(`${id}`);
+
+  const isHidden = targetVideo.classList.contains('hidden-video');
+
+  if (isHidden) {
+    // hidden -> show.
+    targetElement.src = '/fonts/mint-video.svg';
+    targetVideo.classList.remove('hidden-video');
+  } else {
+    // show -> hidden
+    targetElement.src = '/fonts/mint-video-slash.svg';
+    targetVideo.classList.add('hidden-video');
+  }
 }
 
 function toggleConfig(show) {
-    showConfigModal = (show === undefined) ? !showConfigModal : show;
-    if (showConfigModal) {
-        configModal.setAttribute('style', 'display:flex');
-    } else {
-        configModal.setAttribute('style', 'display:none');
-    }
+  const target = $('.local-config-modal');
+
+  const currentOpacity = target.css('opacity');
+  const nextOpacity = show !== undefined ? Number(show) : (Number(currentOpacity) + 1) % 2;
+
+  if (nextOpacity === 1) {
+    target.css('display', 'block');
+    target.animate(
+      {
+        opacity: nextOpacity,
+      },
+      200,
+    );
+    return;
+  }
+
+  target.animate(
+    {
+      opacity: nextOpacity,
+    },
+    400,
+  );
+  target.css('display', 'none');
 }
 
 function toggleUserList(show) {
-    showUserList = (show === undefined) ? !showUserList : show;
+  if (show === undefined) {
+    $('.section-peer-list').fadeToggle('fast');
+    return;
+  }
 
-    if (showUserList) {
-        peerListSection.setAttribute('style', 'display:flex');
-    } else {
-        peerListSection.setAttribute('style', 'display:none');
-    }
+  if (show) {
+    $('.section-peer-list').fadeIn('fast');
+  } else {
+    $('.section-peer-list').fadeOut('fast');
+  }
 }
 
 function toggleMute() {
-    for (const track of localStream.getAudioTracks()) {
-        track.enabled = !track.enabled;
-        const buttonStatus = track.enabled ? '/fonts/microphone.svg' : '/fonts/microphone-slash.svg';
-        const buttonImage = document.getElementById('muteButtonImage');
-        buttonImage.src = buttonStatus;
-    }
+  for (const track of localStream.getAudioTracks()) {
+    track.enabled = !track.enabled;
+    const buttonStatus = track.enabled ? '/fonts/microphone.svg' : '/fonts/microphone-slash.svg';
+    const buttonImage = document.getElementById('muteButtonImage');
+    buttonImage.src = buttonStatus;
+  }
 }
 
 function toggleVideo() {
-    for (const track of localStream.getVideoTracks()) {
-        track.enabled = !track.enabled;
-        const buttonStatus = track.enabled ? '/fonts/video.svg' : '/fonts/video-slash.svg';
-        const buttonImage = document.getElementById('videoButtonImage');
-        buttonImage.src = buttonStatus;
-    }
+  for (const track of localStream.getVideoTracks()) {
+    track.enabled = !track.enabled;
+    const buttonStatus = track.enabled ? '/fonts/video.svg' : '/fonts/video-slash.svg';
+    const buttonImage = document.getElementById('videoButtonImage');
+    buttonImage.src = buttonStatus;
+  }
 }
 
 function addPeerList(id, userInfo) {
-    users[id] = userInfo;
-    const value = users[id];
+  users[id] = userInfo;
+  const value = users[id];
 
-    const container = document.createElement('div');
-    container.className = 'a-user-container';
-    container.id = `peerList_${id}`;
+  const container = document.createElement('div');
+  container.className = 'a-user-container';
+  container.id = `peerList_${id}`;
 
-    const profileImg = document.createElement('img');
-    profileImg.src = value.profile;
-    profileImg.className = 'profile-img';
+  const profileGroup = document.createElement('div');
+  profileGroup.className = 'a-user-profile-group';
 
-    const nameTag = document.createElement('span');
-    nameTag.innerHTML = value.name;
-    nameTag.className = 'profile-name';
+  const profileImg = document.createElement('img');
+  profileImg.src = value.profile;
+  profileImg.className = 'profile-img';
 
-    container.appendChild(profileImg);
-    container.appendChild(nameTag);
-    peerListContainer.appendChild(container);
+  const nameTag = document.createElement('span');
+  nameTag.innerHTML = value.name;
+  nameTag.className = 'profile-name';
+
+  profileGroup.append(profileImg, nameTag);
+  container.append(profileGroup);
+  peerListContainer.append(container);
 }
 
 function deletePeerList(id) {
-    if (!users[id]) {
-        return;
-    }
-    const targetContainer = document.querySelector(`#peerList_${id}`);
+  if (!users[id]) {
+    return;
+  }
+  const targetContainer = document.querySelector(`#peerList_${id}`);
 
-    while (targetContainer.hasChildNodes()) {
-        targetContainer.removeChild(targetContainer.firstChild);
-    }
+  while (targetContainer.hasChildNodes()) {
+    targetContainer.removeChild(targetContainer.firstChild);
+  }
 
-    targetContainer.parentNode.removeChild(targetContainer);
+  targetContainer.parentNode.removeChild(targetContainer);
 
-    delete users[id];
+  delete users[id];
 }
 
 function addWaitList(id) {
-    const value = waitUsers[id];
+  const value = waitUsers[id];
 
-    const container = document.createElement('div');
-    container.className = 'a-user-container';
-    container.id = `waitList_${id}`;
+  const container = document.createElement('div');
+  container.className = 'a-user-container';
+  container.id = `waitList_${id}`;
 
-    const profileImg = document.createElement('img');
-    profileImg.src = value.profile;
-    profileImg.className = 'profile-img';
+  const profileGroup = document.createElement('div');
+  profileGroup.className = 'a-user-profile-group';
 
-    const nameTag = document.createElement('span');
-    nameTag.innerHTML = value.name;
-    nameTag.className = 'profile-name';
+  const buttonGroup = document.createElement('div');
+  buttonGroup.className = 'a-user-button-grouop';
 
-    const approveButton = document.createElement('button');
-    approveButton.innerHTML = '수락';
-    approveButton.className = 'approve-button';
-    approveButton.id = id;
-    approveButton.addEventListener('click', handleApprove);
+  const profileImg = document.createElement('img');
+  profileImg.src = value.profile;
+  profileImg.className = 'profile-img';
 
-    const rejectButton = document.createElement('button');
-    rejectButton.innerHTML = '거절';
-    rejectButton.className = 'reject-button';
-    rejectButton.id = id;
-    rejectButton.addEventListener('click', handleReject);
+  const nameTag = document.createElement('span');
+  nameTag.innerHTML = value.name;
+  nameTag.className = 'profile-name';
 
-    container.appendChild(approveButton);
-    container.appendChild(rejectButton);
-    container.appendChild(profileImg);
-    container.appendChild(nameTag);
-    waitUserContainer.appendChild(container);
+  const approveButton = document.createElement('button');
+  approveButton.innerHTML = '수락';
+  approveButton.className = 'approve-button';
+  approveButton.id = id;
+  approveButton.addEventListener('click', handleApprove);
+
+  const rejectButton = document.createElement('button');
+  rejectButton.innerHTML = '거절';
+  rejectButton.className = 'reject-button';
+  rejectButton.id = id;
+  rejectButton.addEventListener('click', handleReject);
+
+  profileGroup.append(profileImg, nameTag);
+  buttonGroup.append(approveButton, rejectButton);
+
+  container.append(profileGroup, buttonGroup);
+  waitUserContainer.appendChild(container);
 }
 
 function deleteWaitList(id) {
-    if (!waitUsers[id]) {
-        return;
-    }
-    
-    const targetContainer = document.querySelector(`#waitList_${id}`);
-    while (targetContainer.hasChildNodes()) {
-        targetContainer.removeChild(targetContainer.firstChild);
-    }
+  if (!waitUsers[id]) {
+    return;
+  }
 
-    targetContainer.parentNode.removeChild(targetContainer);
+  const targetContainer = document.querySelector(`#waitList_${id}`);
+  while (targetContainer.hasChildNodes()) {
+    targetContainer.removeChild(targetContainer.firstChild);
+  }
 
-    delete waitUsers[id];
+  targetContainer.parentNode.removeChild(targetContainer);
+
+  delete waitUsers[id];
 }
 
 function createParticipantsContainer(id, userInfo, stream) {
-    /* create name tag */
-    let nameTag = document.createElement('span');
-    nameTag.className = 'user-name';
-    nameTag.innerText = userInfo.name;
+  /* create name tag */
+  let nameTag = document.createElement('span');
+  nameTag.className = 'user-name';
+  nameTag.innerText = userInfo.name;
 
-    /* create video */
-    let newVid = document.createElement('video');
-    newVid.srcObject = stream;
-    streams[id] = stream;
-    console.log('stream : ', stream);
-    newVid.id = id;
-    newVid.playsinline = false;
-    newVid.autoplay = true;
+  /* create video */
+  let newVid = document.createElement('video');
+  newVid.srcObject = stream;
+  streams[id] = stream;
+  console.log('stream : ', stream);
+  newVid.id = id;
+  newVid.playsinline = false;
+  newVid.autoplay = true;
 
-    /* create container */
-    let videoContainer = document.createElement('div');
-    videoContainer.className = 'video-container';
-    videoContainer.id = `container_${id}`;
+  /* create container */
+  let videoContainer = document.createElement('div');
+  videoContainer.className = 'video-container';
+  videoContainer.id = `container_${id}`;
 
-    /* create buttons container */
-    let buttonsContainer = document.createElement('div');
-    buttonsContainer.className = 'buttons-container'
-    
-    // create speaker icon 
-    const speakerIcon = document.createElement('img');
-    speakerIcon.src = '/fonts/volume-high.svg';
-    speakerIcon.className = 'video-icons';
-    speakerIcon.id = `speakerButton_${id}`;
-    speakerIcon.addEventListener('click', toggleAudioOutput)
-    buttonsContainer.appendChild(speakerIcon);
+  /* create buttons container */
+  let buttonsContainer = document.createElement('div');
+  buttonsContainer.className = 'buttons-container';
 
-    // create video icon
-    const videoOutputToggleIcon = document.createElement('img');
-    videoOutputToggleIcon.src = '/fonts/mint-video.svg';
-    videoOutputToggleIcon.className = 'video-icons';
-    videoOutputToggleIcon.id = `videoOutputToggleButton_${id}`;
-    videoOutputToggleIcon.addEventListener('click', toggleVideoOutput)
-    buttonsContainer.appendChild(videoOutputToggleIcon);
+  // create speaker icon
+  const speakerIcon = document.createElement('img');
+  speakerIcon.src = '/fonts/volume-high.svg';
+  speakerIcon.className = 'video-icons';
+  speakerIcon.id = `speakerButton_${id}`;
+  speakerIcon.addEventListener('click', toggleAudioOutput);
+  buttonsContainer.appendChild(speakerIcon);
 
-    // create maximize icon
-    const maximzeIcon = document.createElement('img');
-    maximzeIcon.src = '/fonts/maximize-solid.svg';
-    maximzeIcon.className = 'video-icons';
-    maximzeIcon.id = `maximizeButton_${id}`;
-    maximzeIcon.addEventListener('click', handleMaximize);
-    buttonsContainer.appendChild(maximzeIcon);
+  // create video icon
+  const videoOutputToggleIcon = document.createElement('img');
+  videoOutputToggleIcon.src = '/fonts/mint-video.svg';
+  videoOutputToggleIcon.className = 'video-icons';
+  videoOutputToggleIcon.id = `videoOutputToggleButton_${id}`;
+  videoOutputToggleIcon.addEventListener('click', toggleVideoOutput);
+  buttonsContainer.appendChild(videoOutputToggleIcon);
 
-    // create meter
-    let meter = createSoundMeter(id, stream);
-    
-    // append children
-    videosConainerGroup.appendChild(videoContainer);
-    videoContainer.appendChild(meter);
-    videoContainer.appendChild(newVid);
-    videoContainer.appendChild(nameTag);
-    videoContainer.appendChild(buttonsContainer);
+  // create maximize icon
+  const maximzeIcon = document.createElement('img');
+  maximzeIcon.src = '/fonts/maximize-solid.svg';
+  maximzeIcon.className = 'video-icons';
+  maximzeIcon.id = `maximizeButton_${id}`;
+  maximzeIcon.addEventListener('click', handleMaximize);
+  buttonsContainer.appendChild(maximzeIcon);
+
+  // create meter
+  let meter = createSoundMeter(id, stream);
+
+  // append children
+  videosConainerGroup.appendChild(videoContainer);
+  videoContainer.appendChild(meter);
+  videoContainer.appendChild(newVid);
+  videoContainer.appendChild(nameTag);
+  videoContainer.appendChild(buttonsContainer);
 }
 
-function hideAllModal(){
-    toggleConfig(false);
-    toggleUserList(false);
+function hideAllModal() {
+  toggleConfig(false);
+  toggleUserList(false);
 }
 
-videosConainerGroup.addEventListener('click', hideAllModal);
-maximizedVideoSection.addEventListener('click', hideAllModal);
+// videosConainerGroup.addEventListener('click', hideAllModal);
+// maximizedVideoSection.addEventListener('click', hideAllModal);
+
+const horizonal = document.querySelector('.horizonal-flex');
+horizonal.addEventListener('click', (e) => {
+  toggleConfig(false);
+});
